ace3548ea8f50df9d9cb36d6568a1d199a95c21b
kossgreim <ikossgreim@gmail.com>
2013-09-13 00:41:18 +0300

    add current_user to account_update
    This fix problem (current password can't be blank).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:name, :user_name, :avatar, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:name, :user_name, :avatar, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:name, :user_name, :avatar, :email, :password, :current_password) }
   end
 end
