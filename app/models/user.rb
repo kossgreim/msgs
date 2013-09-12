5b9b043035b5d96a8ad6b5d06b62acc07ddfdc98
kossgreim <ikossgreim@gmail.com>
2013-09-13 00:15:06 +0300

    add name, user_name fields and set up to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  			:default_url => "/images/:style/missing.png"
+
+  validates :name, :user_name, presence: true
+  
 end
