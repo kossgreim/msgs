48a2b728965483178d0b5138ae77dee7bb6897f4
kossgreim <ikossgreim@gmail.com>
2013-09-13 17:31:47 +0300

    improve add_if_not_current

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,10 +3,15 @@ module UsersHelper
 	def add_if_user_not_current(user, current_user)
 		if user_signed_in?
 			if user.id != current_user[:id]
-				link_to "Add Friend", friendships_path(:friend_id => user), :method => :post 
+				if Friendship.exists? friend_id: user.id
+					link_to "remove", friends_path, :method => :delete 
+				else
+					link_to "Add Friend", friendships_path(:friend_id => user), :method => :post 
+				end
 			else
 				link_to "edit profile", edit_user_registration_path
 			end
 		end
 	end
+	
 end
