cc7bf6e95793cb3f6481a09be4e2b0368c570a4f
kossgreim <ikossgreim@gmail.com>
2013-09-14 14:42:40 +0300

    generate models for conversation

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913122136) do
+ActiveRecord::Schema.define(version: 20130914114148) do
+
+  create_table "conversations", force: true do |t|
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "conversation_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_conversations", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "conversation_id"
+    t.boolean  "deleted"
+    t.boolean  "read"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
